82025cd6abb5d74cb6390646aacd3fd131ddaa59
MostafaHirsi <mostafa_hirsi@outlook.com>
2020-05-15 19:58:22 +0100

    set up themes, colors and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'theme/colors.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    DateTime dateTime = DateTime.now();
+    bool isDay = dateTime.hour < 21;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+              decorationColor: Colors.white,
+              displayColor: Colors.white,
+            ),
+        fontFamily: 'OpenSans',
+        canvasColor:
+            !isDay ? AppColors.themeColorLight : AppColors.themeColorDark,
+        appBarTheme: AppBarTheme(
+          color: Colors.transparent,
+          elevation: 0,
+        ),
       ),
       home: HomePage(),
     );
@@ -23,8 +39,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-    );
+    return Scaffold();
   }
 }
---
+++ lib/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color themeColorLight = Color(0xFFD7E4F4);
+  static final Color themeColorDark = Color(0xFF10113B);
+}
